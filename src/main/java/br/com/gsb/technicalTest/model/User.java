e4ba20264eeadc37f2dcafe79fc83c03d52fc598
Natã <natagrecu34@gmail.com>
2022-06-25 09:21:12 -0300

    Add JPA anotations
    Signed-off-by: Natã <natagrecu34@gmail.com>

--- src/main/java/br/com/gsb/technicalTest/model/User.java
+++ src/main/java/br/com/gsb/technicalTest/model/User.java
@@ -3,15 +3,38 @@ package br.com.gsb.technicalTest.model;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "user")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    
+    @NotNull
+    @Size(min = 5, max = 100)
     private String name;
+    
+    @NotNull
+    @Size(min = 5, max = 100)
     private String email;
+    
     private char gender;
+    
+    @Size(min = 13, max = 13)
     private String phone;
+    
     private Date birthDate;
 
     public User(long id, String name, String email, char gender, String phone, Date birthDate) {
