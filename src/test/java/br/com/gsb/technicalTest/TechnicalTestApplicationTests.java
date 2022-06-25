2543e6881529642c30443a7f81645b9ab69e91bd
Natã <natagrecu34@gmail.com>
2022-06-25 08:34:58 -0300

    Initial commit, project structure.
    Signed-off-by: Natã <natagrecu34@gmail.com>

---
+++ src/test/java/br/com/gsb/technicalTest/TechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gsb.technicalTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
